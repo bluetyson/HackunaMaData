02393c6d1adc036465616e343ad10e9c043b3edd
Madan Parameswaran <madan.mesh@gmail.com>
2017-07-29 15:27:42 +0930

    FIX: file path childcare

--- BizKit/childcare.R
+++ BizKit/childcare.R
@@ -1,6 +1,6 @@
 library(readr)
 
-child.care <- read_csv("~/Downloads/childcare.csv")
+child.care <- read_csv("./data/csv/childcare.csv")
 child.care <- child.care[,c(2,4,11)]
 
 child.care <- cbind(child.care, sapply (strsplit(child.care$`Physical Address`,","),`[`, 2))
